f5642b4eab429e151529970fa8ee7783e1b325a8
Bastien Van Houdt <bastienvanhoudt@gmail.com>
2023-03-28 11:26:45 +0200

    refactor Address to be Record

--- src/main/java/com/bastienvh/eurder/domain/Address.java
+++ src/main/java/com/bastienvh/eurder/domain/Address.java
@@ -2,21 +2,14 @@ package com.bastienvh.eurder.domain;
 
 import org.apache.commons.lang3.Validate;
 
-public class Address {
-    private final String street;
-    private final String number;
-    private final String city;
-    private final int postalCode;
+import java.util.Objects;
 
-    public Address(String street, String number, String city, int postalCode) {
-        Validate.notNull(street, "street can't be null");
-        Validate.notNull(number, "number can't be null");
-        Validate.notNull(city, "city can't be null");
+public record Address (String street, String number, String city, int postalCode) {
+    public Address {
+        Objects.requireNonNull(street, "street can't be null");
+        Objects.requireNonNull(number, "number can't be null");
+        Objects.requireNonNull(city, "city can't be null");
         Validate.exclusiveBetween(999, 10000, postalCode, "postal code is not valid");
-        this.street = street;
-        this.number = number;
-        this.city = city;
-        this.postalCode = postalCode;
     }
 
 }
